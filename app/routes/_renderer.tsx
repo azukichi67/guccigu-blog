6b0f19ee8b73aa27c7a39fe0456df3742c438976
azukichi67 <>
2025-02-16 13:57:49 +0900

    add linter

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,19 +1,19 @@
-import { Style } from 'hono/css'
-import { jsxRenderer } from 'hono/jsx-renderer'
-import { Script } from 'honox/server'
+import { Style } from "hono/css";
+import { jsxRenderer } from "hono/jsx-renderer";
+import { Script } from "honox/server";
 
 export default jsxRenderer(({ children, title }) => {
   return (
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <Script src="/app/client.ts" async />
+        <link href="/favicon.ico" rel="icon" />
+        <Script async src="/app/client.ts" />
         <Style />
       </head>
       <body>{children}</body>
     </html>
-  )
-})
+  );
+});
