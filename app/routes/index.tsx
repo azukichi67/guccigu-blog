6476ab9c69853716ea120accca2b7a1adb610395
azukichi67 <>
2025-02-16 14:43:34 +0900

    add mdx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,12 +6,33 @@ const className = css`
   font-family: sans-serif;
 `;
 
+export type Meta = {
+  title: string;
+};
+
 export default createRoute((c) => {
+  const posts = import.meta.glob<{ frontmatter: Meta }>("./posts/*.mdx", {
+    eager: true,
+  });
   const name = c.req.query("name") ?? "Hono";
   return c.render(
     <div class={className}>
       <h1 class="text-red-500">Hello, {name}!</h1>
       <Counter />
+      <h2>Posts</h2>
+      <ul class="article-list">
+        {Object.entries(posts).map(([id, module], i) => {
+          if (module.frontmatter) {
+            return (
+              <li key={i}>
+                <a href={`${id.replace(/\.mdx$/, "")}`}>
+                  {module.frontmatter.title}
+                </a>
+              </li>
+            );
+          }
+        })}
+      </ul>
     </div>,
     { title: name }
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import build from "@hono/vite-build/cloudflare-pages";
 import adapter from "@hono/vite-dev-server/cloudflare";
+import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     honox({ devServer: { adapter } }),
+    mdx({
+      jsxImportSource: "hono/jsx",
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    }),
     build(),
     tailwindcss(),
     tsconfigPaths(),
