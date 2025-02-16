9c06bad3fde8a5fd1b253cdffb3c9cb9f5748f36
azukichi67 <>
2025-02-16 13:36:18 +0900

    init honox

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import build from "@hono/vite-build/cloudflare-pages";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [honox({ devServer: { adapter } }), build()],
+  server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
+});
